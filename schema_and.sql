86f3d2f0115c9a28bde3e3d3274e73387f25a399
jubberman <37309664+jubberman@users.noreply.github.com>
2020-09-26 21:47:05 +0100

    Create schema_and.sql

---
+++ schema_and.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Persons 
+    (PersonID INTEGER NOT NULL AUTO_INCREMENT,
+     Name VARCHAR(40),
+     DoB DATE,
+     PRIMARY KEY (PersonID));
+
+CREATE TABLE Cast 
+    (FilmID INTEGER NOT NULL,
+     ActorID INTEGER NOT NULL,
+     PRIMARY KEY (FilmID, ActorID),
+     FOREIGN KEY (FilmID) REFERENCES Films(FilmID) ON DELETE RESTRICT,     
+     FOREIGN KEY (ActorID) REFERENCES Persons(PersonID) ON DELETE RESTRICT);
+
+CREATE TABLE Theatres
+    (TheatreID INTEGER NOT NULL AUTO_INCREMENT,
+     Name VARCHAR(40),
+     Screens INTEGER,
+     Location VARCHAR(40),
+     PRIMARY KEY (TheatreID));
+
+CREATE TABLE Screens
+    (ScreenID INTEGER NOT NULL AUTO_INCREMENT,
+     Theatre INTEGER NOT NULL,
+     ScreenNumber INTEGER,
+     PRIMARY KEY (ScreenID),
+     FOREIGN KEY (Theatre) REFERENCES Theatres(TheatreID) ON DELETE RESTRICT);
+
+CREATE TABLE Showtimes
+    (Film INTEGER NOT NULL,
+     Screen INTEGER NOT NULL,
+     ShowTime DATETIME,
+     PRIMARY KEY (Film, Screen, ShowTime),
+     FOREIGN KEY (Film) REFERENCES Films(FilmID) ON DELETE RESTRICT,
+     FOREIGN KEY (Screen) REFERENCES Screens(ScreenID) ON DELETE RESTRICT);
